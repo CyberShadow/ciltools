980010bed80d6db2b78922cb795f4a217ab1b3fa
Vladimir Panteleev <git@thecybershadow.net>
2016-06-21 15:11:18 +0000

    ilref: Use regex

--- ilref.d
+++ ilref.d
@@ -1,11 +1,13 @@
 import std.algorithm;
 import std.file;
 import std.path;
+import std.regex;
 import std.stdio;
 import std.string;
 
 import ae.utils.funopt;
 import ae.utils.main;
+import ae.utils.regex;
 
 struct ILRef
 {
@@ -13,11 +15,12 @@ static:
 	@("Rename a method")
 	void renameMethod(string className, string oldName, string newName)
 	{
+		auto reCall = regex(`\b` ~ escapeRE(className ~ "::" ~ oldName) ~ `\b`);
 		foreach (de; dirEntries("", "*.il", SpanMode.depth))
 		{
 			auto os = de.readText();
 			auto s = os;
-			s = s.replace(className ~ "::" ~ oldName, className ~ "::" ~ newName);
+			s = s.replaceAll(reCall, className ~ "::" ~ newName);
 			if (de.baseName == className ~ ".class.il")
 				s = s.replace(" " ~ oldName ~ "(", " " ~ newName ~ "(");
 			if (os != s)
