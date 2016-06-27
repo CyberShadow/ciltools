c8231c4dd30f7b4a0d48ed4f93dfc42c2d616355
Vladimir Panteleev <git@thecybershadow.net>
2016-06-27 12:47:51 +0000

    csstub: Wrangle project/solution files into consistency

--- csstub.d
+++ csstub.d
@@ -1,8 +1,10 @@
 /// Stub out method bodies in a C# project.
 /// Useful for getting decompiled code to partially compile.
+/// Also does some project/solution file edits to make editing/merging easier.
 
 import std.algorithm;
 import std.array;
+import std.exception;
 import std.file;
 import std.path;
 import std.stdio;
@@ -13,13 +15,16 @@ import ae.utils.array;
 import ae.utils.funopt;
 import ae.utils.main;
 import ae.utils.path;
+import ae.utils.textout;
+import ae.utils.xmllite;
+import ae.utils.xmlwriter;
 
-void csstub(string inputDir, string outputDir)
+void csstub(string inputDir, string outputDir, string guid="{01234567-89AB-CDEF-0123-456789ABCDEF}")
 {
 	foreach (de; dirEntries(inputDir, SpanMode.breadth))
 	{
 		stderr.writeln(de.name);
-		auto target = de.name.rebasePath(inputDir, outputDir);
+		auto target = buildPath(outputDir, de.name.absolutePath.relativePath(inputDir.absolutePath).sanitizeFileName());
 		if (de.isDir)
 			mkdirRecurse(target);
 		else
@@ -78,8 +83,46 @@ void csstub(string inputDir, string outputDir)
 			}
 		}
 		else
+		if (de.extension == ".sln")
+		{
+			auto lines = de.name.readText().split("\r\n");
+			foreach (ref line; lines)
+			{
+				// Make GUID stable to facilitate merge
+				if (line.startsWith("Project("))
+				{
+					enforce(line[$-43..$-38] == `", "{`);
+					line = line[0..$-39] ~ guid ~ `"`;
+				}
+				else
+				if (line.startsWith("\t\t{"))
+				{
+					enforce(line[40] == '.');
+					line = "\t\t" ~ guid ~ line[40..$];
+				}
+			}
+			std.file.write(target, lines.join("\r\n"));
+		}
+		else
+		if (de.extension == ".csproj")
+		{
+			auto xml = de.name.readText().xmlParse();
+			xml["Project"]["PropertyGroup", 0]["ProjectGuid"][0].tag = guid;
+			xml["Project"]["ItemGroup", 1].children.sort!((a, b) => a.attributes["Include"] < b.attributes["Include"]);
+			xml["Project"]["ItemGroup", 1].children.each!(child => child.attributes["Include"] = sanitizeFileName(child.attributes["Include"]));
+
+			CustomXmlWriter!(StringBuilder, CustomXmlFormatter!(' ', 2)) writer;
+			xml.writeTo(writer);
+			std.file.write(target, writer.output.get().replace("\n", "\r\n").strip());
+		}
+		else
 			std.file.write(target, std.file.read(de.name));
 	}
+}
+
+string sanitizeFileName(string fn)
+{
+	return fn.replace(" ", "_");
 }
 
 mixin main!(funopt!csstub);
